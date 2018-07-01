8d7b4e97e38bf3d4245596223756289d5f89d7d7
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-01 19:11:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     def kind
         # binding.pry
         # SIDES.sort!
-        if [@leftlength, @rightlength, @bottomlength].all? { |e|  e > 0}
+        if SIDES.all? { |e|  e > 0}
             if leftlength == rightlength && leftlength == bottomlength
                 :equilateral
             elsif (leftlength == rightlength && leftlength + rightlength > bottomlength) ||
